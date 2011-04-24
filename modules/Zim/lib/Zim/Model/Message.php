1bc1c60577e4b0ea2bf9ae05a5c3fb19d7eadd8b
Kyle Giovannetti <078220g@acadiau.ca>
2011-04-24 19:16:21 -0300

    massive changes trying to use doctrine, system is broken until its all cleaned up and working.

---
+++ modules/Zim/lib/Zim/Model/Message.php
@@ -0,0 +1,50 @@
+<?php
+class Zim_Model_Message extends Doctrine_Record
+{
+    public function setTableDefinition()
+    {
+        $this->setTableName('zim_message');
+        $this->hasColumn('mid', 'integer', 16, array(
+        	'unique'  => true,
+            'primary' => true,
+            'notnull' => true,
+            'autoincrement' => true
+        	)
+        );
+        $this->hasColumn('msg_to', 'integer', 16, array(
+        	'unique'  => false,
+            'primary' => false,
+            'notnull' => true
+        	)
+        );
+        $this->hasColumn('msg_from', 'integer', 16, array(
+        	'unique'  => false,
+            'primary' => false,
+            'notnull' => true
+        	)
+        );
+        $this->hasColumn('message', 'clob', array(
+        	'unique' => false,
+        	'primary'=> false,
+            'notnull' => true,
+        	'default' => ''
+        	)
+        );
+        
+    }
+
+    public function setUp()
+    {
+    	$this->actAs('Timestampable');
+        $this->hasOne('Zim_Model_User as to', array(
+                'local' => 'msg_to',
+                'foreign' => 'uid',
+            )
+        );
+        $this->hasOne('Zim_Model_User as from', array(
+                'local' => 'msg_from',
+                'foreign' => 'uid',
+            )
+        );
+    }
+}
