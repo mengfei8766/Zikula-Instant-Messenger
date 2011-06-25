124d35f333d3322f2a60c3abee7c1d331343c006
Kyle Giovannetti <078220g@acadiau.ca>
2011-06-24 23:18:21 -0300

    add history api stub

---
+++ modules/Zim/lib/Zim/Api/History.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Zikula-Instant-Messenger (ZIM)
+ *
+ * @Copyright Kyle Giovannetti 2011
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @author  Kyle Giovannetti
+ * @package Zim
+ */
+
+class Zim_Api_History extends Zikula_AbstractApi {
+
+}
