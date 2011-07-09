4ea51a1712c97105b5243e4ac58589207651dc4c
Kyle Giovannetti <078220g@acadiau.ca>
2011-07-09 12:16:42 -0300

    add group API stub.

---
+++ modules/Zim/lib/Zim/Api/Group.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Zikula-Instant-Messenger (ZIM)
+ *
+ * @Copyright Kyle Giovannetti 2011
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @author  Kyle Giovannetti
+ * @package Zim
+ */
+
+class Zim_Api_Group extends Zikula_AbstractApi
+{
+    /**
+     * Create a new group.
+     */
+    public function create_group() {
+
+    }
+
+    /**
+     * Delete a group.
+     *
+     */
+    public function delete_group() {
+
+    }
+
+    /**
+     * Edit the name of a group.
+     *
+     */
+    public function edit_groupname() {
+
+    }
+
+    /**
+     * Add a user to a group.
+     */
+    public function add_to_group() {
+
+    }
+
+	/**
+     * Delete a user from a group.
+     */
+    public function del_from_group() {
+
+    }
+}
